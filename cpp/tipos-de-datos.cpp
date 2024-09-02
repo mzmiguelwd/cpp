6d5e01a048008482c7bb22e6f7a81d64fb9c77de
mzmiguelwd <128485718+mzmiguelwd@users.noreply.github.com>
2024-09-02 13:19:32 -0500

    Add files via upload

---
+++ cpp/tipos-de-datos.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int entero = 15;
+    float flotante = 15.43;
+    double mayor = 15.3421341;
+    char letra = "a";
+
+    cout << entero << endl;
+    cout << flotante << endl;
+    cout << mayor << endl;
+    cout << letra << endl;
+
+    return 0;
+    
+}
