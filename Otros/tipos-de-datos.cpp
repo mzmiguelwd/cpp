37df0efd196a3c074ad37ec23540a6443dbbf3fb
mzmiguelwd <128485718+mzmiguelwd@users.noreply.github.com>
2024-09-16 12:27:32 -0500

    Add files via upload

---
+++ Otros/tipos-de-datos.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int entero = 15;
+    float flotante = 10.45;
+    double mayor = 16.3456;
+    char letra = 'a';
+    string palabra = "diezletras";
+
+    cout << entero << endl;
+    cout << flotante << endl;
+    cout << mayor << endl;
+    cout << letra << endl;
+    cout << palabra << endl;
+
+    return 0;
+    
+}
